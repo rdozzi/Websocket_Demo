1ed9e75c7b7c25530d8892e82838091c15d544d8
Raphael Dozzi <rdozzi84@gmail.com>
2023-12-25 21:19:33 -0800

    Added http and server varrs

--- websockets.js
+++ websockets.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const http = require('http');
 
 const app = express();
+const server = http.createServer(app);
 
 app.get('/', (req,res) => {
     res.send('Hello World')
