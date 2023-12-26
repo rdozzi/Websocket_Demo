7a0afd039a7eace7263779fb59ffa011fab71a24
Raphael Dozzi <rdozzi84@gmail.com>
2023-12-25 21:31:49 -0800

    Added backend code to sockets.js for socket.io

--- websockets.js
+++ websockets.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+
+// After extracting the Server object from socket.io, we then add our express app server as a new object
+// of the Socket.io server
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    console.log("User Connected")
+})
 
 app.get('/', (req,res) => {
     res.send('Hello World')
