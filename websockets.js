ee463d8f3e982969d9c8946660d5980d225468f2
Raphael Dozzi <rdozzi84@gmail.com>
2023-12-25 22:02:21 -0800

    Added static line to express

--- websockets.js
+++ websockets.js
@@ -3,6 +3,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 
 const app = express();
+app.use(express.static("public")); // express.static files in the named directory will act as static resources
 const server = http.createServer(app);
 
 // After extracting the Server object from socket.io, we then add our express app server as a new object
@@ -13,9 +14,9 @@ io.on("connection", (socket) => {
     console.log("User Connected")
 })
 
-app.get('/', (req,res) => {
-    res.send('Hello World')
-})
+// app.get('/', (req,res) => {
+//     res.send('Hello World')
+// })
 
 app.listen(3000,() => {
     console.log("Server Started")
