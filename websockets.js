568e8de9ef89e5f22524fb6befe3d09df18fbe07
Raphael Dozzi <rdozzi84@gmail.com>
2023-12-25 22:09:55 -0800

    Added client-side scripts to index

--- websockets.js
+++ websockets.js
@@ -12,6 +12,7 @@ const io = new Server(server);
 
 io.on("connection", (socket) => {
     console.log("User Connected")
+    socket.emit("Message: ", "Welcome from Server")
 })
 
 // app.get('/', (req,res) => {
