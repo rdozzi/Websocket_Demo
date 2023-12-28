15c6b671a5948ac3fe4f9df9dd34d296d84580bc
Raphael Dozzi <rdozzi84@gmail.com>
2023-12-28 09:30:54 -0800

    Updated content for btn event listener

--- public/script.js
+++ public/script.js
@@ -1,8 +1,20 @@
 const socket = io(); // Available in global scope because of server side code
-const btn = document.getElementById("btn"); // Recall that you should call elements by id
+const btn = document.getElementById("send"); // Recall that you should call elements by id
 const input = document.getElementById("message");
+const ul = document.getElementById("list");
 
 btn.addEventListener("click", () =>{
+    const value = input.value;
+    const div = document.createElement("div");
+    div.setAttribute("class","sender");
+    const li = document.createElement("li");
+    li.innerText = value;
+    const para = document.createElement("p");
+    para.innerText = "sender";
+    div.appendChild(para);
+    div.appendChild(li);
+    ul.appendChild(div);
+
     console.log("sending message", input.value);
     socket.emit("message", input.value);
     input.value="";
